2583536fab0c8ef8ca4c00b7a26f02777ca6fec3
teampacifica2 <patrick.urbanski@ca-pacifica.fr>
2018-10-10 09:51:49 +0200

    feat(docker) add health-check

---
+++ containerbank-api/src/main/java/org/hackathon/packapp/containerbank/web/HealthCheckController.java
@@ -0,0 +1,13 @@
+package org.hackathon.packapp.containerbank.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/health-check")
+    public String healthcheck() {
+        return "Application started.....";
+    }
+}
